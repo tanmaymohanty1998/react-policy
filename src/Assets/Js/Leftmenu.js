1fb95525178cb6c7d04d7ccc2dd1062a531d135b
Tanmay Mohanty <Tanmaymohanty1998@gmail.com>
2023-01-18 16:27:02 +0530

    left menu function added

---
+++ src/Assets/Js/Leftmenu.js
@@ -0,0 +1,350 @@
+const LeftMenuJson = [
+
+    {
+        id: "f_01",
+        title: "setting",
+        icon: "fa-cog",
+        Child: [
+            {
+                id: "s_01_01",
+                title: "pages",
+                SubChild: [
+                    {
+                        id: "t_01_01_01",
+                        title: "page url",
+                        pageurl: "/hyhyhy"
+                    }
+                ]
+            },
+            {
+                id: "s_01_02",
+                title: "logs",
+                SubChild: [
+                    {
+                        id: "t_01_02_02",
+                        title: "Audit Log",
+                    }
+                ]
+            }
+        ]
+
+    },
+    {
+        id: "f_02",
+        title: "IAM",
+        icon: "fa-user ",
+        Child: [
+            {
+                id: "s_02_01",
+                title: "Access Management",
+                SubChild: [
+                    {
+
+                        id: "t_02_01_01",
+                        title: "User Role",
+                        pageurl: "pages/v-w user role"
+
+                    },
+                    {
+
+                        id: "t_02_01_02",
+                        title: "User Role",
+
+                    },
+                    {
+
+                        id: "t_02_01_03",
+                        title: "User Role",
+
+                    },
+                    {
+
+                        id: "t_02_01_04",
+                        title: "User Role",
+
+                    },
+                    {
+
+                        id: "t_02_01_05",
+                        title: "User Role",
+
+                    },
+                ]
+            },
+            {
+                id: "s_02_02",
+                title: "Entitiy Credentials",
+                SubChild: [
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Entity Credentials",
+                    }
+                ]
+
+            }
+        ]
+    },
+    {
+        id: "f_03",
+        title: "Master",
+        icon: "fa-cogs",
+        Child: [
+            {
+                id: "s_03_01",
+                title: "Motor",
+                SubChild: [
+                    {
+
+                        id: "t_03_01_01",
+                        title: "Manufacture",
+                        pageurl: "pages/v-w user role"
+
+                    },
+                    {
+
+                        id: "t_03_01_02",
+                        title: "Model",
+
+                    },
+                    {
+
+                        id: "t_03_01_03",
+                        title: "Variant",
+
+                    },
+                    {
+
+                        id: "t_03_01_04",
+                        title: "Transmission Type",
+
+                    },
+                    {
+
+                        id: "t_02_01_05",
+                        title: "Color",
+
+                    },
+                    {
+
+                        id: "t_02_01_01",
+                        title: "Fuel Type",
+                        pageurl: "pages/v-w user role"
+
+                    },
+                    {
+
+                        id: "t_02_01_02",
+                        title: "Body Type",
+
+                    },
+                    {
+
+                        id: "t_02_01_03",
+                        title: "Ex Showroom Price",
+
+                    },
+                    {
+
+                        id: "t_02_01_04",
+                        title: "RTO Location",
+
+                    },
+                    {
+
+                        id: "t_02_01_05",
+                        title: "Depreciation Rate",
+
+                    },
+                    {
+
+                        id: "t_02_01_01",
+                        title: "Product Discount",
+                        pageurl: "pages/v-w user role"
+
+                    },
+                    {
+
+                        id: "t_02_01_02",
+                        title: "Discount Type",
+
+                    },
+                    {
+
+                        id: "t_02_01_03",
+                        title: "Premium Rate",
+
+                    },
+                    {
+
+                        id: "t_02_01_04",
+                        title: "Manufacturer Model",
+
+                    },
+                    {
+
+                        id: "t_02_01_05",
+                        title: "Model Variant",
+
+                    },
+                    {
+
+                        id: "t_02_01_01",
+                        title: "Variant Transmission Type",
+                        pageurl: "pages/v-w user role"
+
+                    },
+                    {
+
+                        id: "t_02_01_02",
+                        title: "Variant Color",
+
+                    },
+                    {
+
+                        id: "t_02_01_03",
+                        title: "Variant Fuel Type",
+
+                    },
+                    {
+
+                        id: "t_02_01_04",
+                        title: "Variant Body Type",
+
+                    },
+                    {
+
+                        id: "t_02_01_05",
+                        title: "Salvag Vendor",
+
+                    },
+                    {
+
+                        id: "t_02_01_05",
+                        title: "Vehicle Info",
+
+                    },
+                ]
+            },
+            {
+                id: "s_02_02",
+                title: "Marine",
+                SubChild: [
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Commodity Type",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Conveyance Mode",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Voyage Type",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Excess Type",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Storage Type",
+                    }
+                ]
+            },
+            {
+                id: "s_02_02",
+                title: "Fire",
+                SubChild: [
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Occupancy",
+                    }
+                ]
+
+
+            },
+            {
+                id: "s_02_02",
+                title: "Motor Claim",
+                SubChild: [
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Loss Location",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Spare Parts",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Catastrophe",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Claim Other Details",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Count Type",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Court",
+                    }
+                ]
+
+            },
+            {
+                id: "s_02_02",
+                title: "Generic",
+                SubChild: [
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Entity Credentials",
+                    }
+                ]
+
+            },
+            {
+                id: "s_02_02",
+                title: "Output",
+                SubChild: [
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Letter Config",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "Output Process",
+                    },
+                    {
+
+                        id: "t_02_02_01",
+                        title: "SMS Config",
+                    }
+                ]
+
+            }
+        ]
+
+    },
+
+
+]
+export default LeftMenuJson;
